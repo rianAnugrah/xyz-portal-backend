ab8f180fe42cae25a097d193293b033a1a90b56d
Rian Anugrah <rian.te.quiero@gmail.com>
2025-03-12 14:12:42 +0700

    Fix ENV

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
-import authRoutes from "./authRoutes";
+
 import routes from "./routes";
+import authRoutes from "./routes/auth";
 
 const fastify = Fastify({ logger: true });
 
@@ -15,6 +16,8 @@ const start = async () => {
   try {
     await fastify.listen({ port: 3000, host: "0.0.0.0" }); // Menambahkan host: '0.0.0.0'
     console.log("Server running on http://0.0.0.0:3000");
+    console.log(`Server running on ${process.env.ENV} environment`);
+    console.log(`Server running on ${process.env.SUPABASE_URL} supabase`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
