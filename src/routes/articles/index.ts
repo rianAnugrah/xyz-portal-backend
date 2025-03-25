66569c9b4ac9d66888be0da0a9278c02a06d6f4c
Rian Anugrah <rian.te.quiero@gmail.com>
2025-03-25 08:03:17 +0700

    read article by slug

--- src/routes/articles/index.ts
+++ src/routes/articles/index.ts
@@ -4,13 +4,14 @@ import { createArticle } from "./create";
 
 import { updateArticle } from "./update";
 import { deleteArticle } from "./delete";
-import { readArticleSingle } from "./read-single";
+import { readArticleBySlug, readArticleSingle } from "./read-single";
 import { readArticleBatch } from "./read-batch";
 
 export default async function articleRoutes(fastify: FastifyInstance) {
   await Promise.all([
     createArticle(fastify),
     readArticleSingle(fastify),
+    readArticleBySlug(fastify),
     readArticleBatch(fastify),
     updateArticle(fastify),
     deleteArticle(fastify),
